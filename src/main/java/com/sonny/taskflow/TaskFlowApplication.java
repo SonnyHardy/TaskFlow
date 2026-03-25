1fa7f7ad3600e6f2715d9f0bbe6314e5433db9c3
SonnyHardy <htambeck@gmail.com>
2026-03-25 21:37:37 +0100

    initialize Spring Boot project

---
+++ src/main/java/com/sonny/taskflow/TaskFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaskFlowApplication.class, args);
+    }
+
+}
